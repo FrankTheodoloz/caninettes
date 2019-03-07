71db52cd40d82d498bd0beb18afa77b4ae63ed43
ESIG-318-14\DE_OLIVEIRA_DAV-ESIG <elv-david.dlvrr@eduge.ch>
2019-03-07 16:21:00 +0100

    Mise à jour test unitaire

--- testUnitaire/TestUnitaireListeCaninette.java
+++ testUnitaire/TestUnitaireListeCaninette.java
@@ -7,10 +7,11 @@ import static junit.framework.TestCase.assertEquals;
 
 public class TestUnitaireListeCaninette {
 
-    /** Tester la connexion à la base de données et le nombre d'enregistrements stockés**/
+    /** Tester la connexion à la base de données et le nombre d'enregistrements stockés, sur une base de données test**/
     @Test public void nbEnregistrementValide() throws SQLException {
         DaoCanninettes daoCanninettes = new DaoCanninettes("jdbc:sqlite:mydatabaseTest.db");
         int nbEnregistrement = daoCanninettes.afficherCaninette().size();
+        /** Le nombre d'enregistrements de la base de données est de 601**/
         assertEquals(601,nbEnregistrement);
     }
 
